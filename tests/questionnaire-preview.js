7544d393de720bc4a86314b71f3ff435bb487dd6
eriksandsten <eriksandsten12@gmail.com>
2017-04-13 14:24:38 +0200

    Updated questionnaire preview tests

--- tests/questionnaire-preview.js
+++ tests/questionnaire-preview.js
@@ -6,33 +6,71 @@ var RUN_QUESTIONNAIRE_PREVIEW_TESTS = true;
 
 if (RUN_QUESTIONNAIRE_PREVIEW_TESTS == true) {
 	$(document).ready(function() {
-		QUnit.module('Preview', {
+		function generateRandomIntegerBetween(min, max) {
+  			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+		QUnit.module('QUESTIONNAIRE PREVIEW', {
 			beforeEach: function(assert) {
-				// Random integer between 1 and 20
-				var random_integer = Math.floor((Math.random() * 20) + 1);
+				var max_number_of_added_sections = 20;
+				var min_number_of_added_sections = max_number_of_added_sections / 2;
+
+				var max_number_of_added_heading_and_descriptions = max_number_of_added_sections / 2;
+				var min_number_of_added_heading_and_descriptions = max_number_of_added_heading_and_descriptions / 2;
+
+				var max_number_of_added_questions = max_number_of_added_sections / 2;
+				var min_number_of_added_questions = max_number_of_added_questions / 2;
+
+				var random_number_of_heading_and_descriptions = generateRandomIntegerBetween(min_number_of_added_heading_and_descriptions, max_number_of_added_heading_and_descriptions);
+				var random_number_of_questions = generateRandomIntegerBetween(min_number_of_added_questions, max_number_of_added_sections);
+
+				/*
+					min <= number_of_added_heading_and_descriptions <= max
+					min <= number_of_added_questions <= max
+				*/
+				assert.strictEqual(random_number_of_heading_and_descriptions >= min_number_of_added_heading_and_descriptions &&
+					random_number_of_heading_and_descriptions <= max_number_of_added_heading_and_descriptions, true);
+				assert.strictEqual(random_number_of_heading_and_descriptions >= min_number_of_added_heading_and_descriptions &&
+					random_number_of_heading_and_descriptions <= max_number_of_added_heading_and_descriptions, true);
+
+				this.number_of_added_heading_and_descriptions = 0;
+				this.number_of_added_questions = 0;
 				this.number_of_added_sections = 0;
 
-				for (var i = 1; i <= random_integer; i++) {
-					// Random section integer between 1 and 2
-					var random_section = Math.floor((Math.random() * 2) + 1);
-
-					// Random section integer should be 1 or 2
-					assert.strictEqual(random_section >= 1 && random_section <= 2, true);
-
-					if (random_section === 1) {
-						assert.ok(questionnaire_add_question());
-						assert.strictEqual(questionnaire_get_sections().length, ++this.number_of_added_sections);
-
-					} else if (random_section === 2) {
-						assert.ok(questionnaire_add_heading_and_description());
-						assert.strictEqual(questionnaire_get_sections().length, ++this.number_of_added_sections);
-					}
+				for (var i = 1; i <= random_number_of_heading_and_descriptions; i++) {
+					assert.ok(questionnaire_add_heading_and_description());
+					this.number_of_added_heading_and_descriptions++;
 				}
+
+				for (var j = 1; j <= random_number_of_questions; j++) {
+					assert.ok(questionnaire_add_question());
+					this.number_of_added_questions++;
+				}
+
+				this.number_of_added_sections = this.number_of_added_heading_and_descriptions + this.number_of_added_questions;
+
+				assert.strictEqual(questionnaire_get_heading_and_descriptions().length, this.number_of_added_heading_and_descriptions);
+				assert.strictEqual(questionnaire_get_questions().length, this.number_of_added_questions);
+				assert.strictEqual(questionnaire_get_sections().length, this.number_of_added_sections);
+
+				this.number_of_assertions_made_before_test = 2 + (1*this.number_of_added_sections) + 3;
 			}
 		});
 
-		QUnit.test('Preview questionnaire heading and description', function(assert) {
-			assert.expect(9*this.number_of_added_sections + 7);
+		QUnit.test('Preview questionnaire', function(assert) {
+			var number_of_preview_states = 2; // Preview mode: on/off
+			var number_of_questionnaire_assertions_made = number_of_preview_states * 3; // Title, description, footer: Preview on/off
+			var number_of_questionnaire_section_assertions_made = number_of_preview_states * (3*this.number_of_added_sections); // Section title, description, bottom menu: Preview on/off
+			var number_of_questionnaire_question_assertions_made = number_of_preview_states * (1*this.number_of_added_questions); // Question type dropdown list: Preview on/off
+
+			var total_number_of_assertions_made =
+				this.number_of_assertions_made_before_test +
+				number_of_questionnaire_assertions_made +
+				number_of_questionnaire_section_assertions_made +
+				number_of_questionnaire_question_assertions_made +
+				1; // One extra assertion for 'questionnaire_preview()'
+
+			assert.expect(total_number_of_assertions_made);
 
 			assert.strictEqual($(questionnaire.title.selector).attr('contenteditable'), 'true');
 			assert.strictEqual($(questionnaire.description.selector).attr('contenteditable'), 'true');
@@ -42,6 +80,10 @@ if (RUN_QUESTIONNAIRE_PREVIEW_TESTS == true) {
 				assert.strictEqual($(section).children('.panel-heading').children('.panel-title').attr('contenteditable'), 'true');
 				assert.strictEqual($(section).children('.panel-body').children('p[class*="-description"]').attr('contenteditable'), 'true');
 				assert.strictEqual($(section).children('.panel-footer').css('display'), 'block');
+			});
+
+			questionnaire_get_questions().each(function(index, question) {
+				assert.strictEqual($(question).children('.panel-heading').children('.questionnaire-question-types-dropdown').css('display'), 'block');
 			});
 
 			// Preview questionnaire
@@ -55,6 +97,10 @@ if (RUN_QUESTIONNAIRE_PREVIEW_TESTS == true) {
 				assert.strictEqual($(section).children('.panel-heading').children('.panel-title').attr('contenteditable'), 'false');
 				assert.strictEqual($(section).children('.panel-body').children('p[class*="-description"]').attr('contenteditable'), 'false');
 				assert.strictEqual($(section).children('.panel-footer').css('display'), 'none');
+			});
+
+			questionnaire_get_questions().each(function(index, question) {
+				assert.strictEqual($(question).children('.panel-heading').children('.questionnaire-question-types-dropdown').css('display'), 'none');
 			});
 		});
 	});
